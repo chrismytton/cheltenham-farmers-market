62c1cb8fb3e61540b4df4c4bd1aabe1d17233300
Chris Mytton <chrismytton@gmail.com>
2018-10-21 12:11:13 +0100

    Combine scripts and switch to public dir output

--- scraper.rb
+++ scraper.rb
@@ -2,10 +2,26 @@ require 'bundler/setup'
 require 'open-uri'
 require 'nokogiri'
 require 'pry'
+require 'icalendar'
+require 'date'
 
 html = Nokogiri(open('https://www.cheltenham.gov.uk/info/24/markets/491/cheltenham_farmers_market').read)
 
 dates = html.search('#content ul').first.search('li').map(&:text)
 dates_parsed = dates.flat_map { |d| month = d.split(' ').last; d.scan(/\d+/).map { |n| "#{n} #{month} 2018" } }.map { |d| Date.parse(d) }
 
-puts dates_parsed
+
+cal = Icalendar::Calendar.new
+
+dates_parsed.each do |date|
+  start_time = DateTime.parse("#{date} 09:00")
+  end_time = DateTime.parse("#{date} 14:00")
+
+  cal.event do |e|
+    e.dtstart     = start_time
+    e.dtend       = end_time
+    e.summary     = "Cheltenham farmers' market"
+  end
+end
+
+File.write('public/farmers-markets.ics', cal.to_ical)
